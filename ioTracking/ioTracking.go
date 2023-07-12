dbd272493cf6c1be8e633804feebd8665c5a2929
Freddie Mather <freddie.mather@smartdcs.co.uk>
2023-07-12 14:42:10 +0000

    Fix timestamp bug

--- ioTracking/ioTracking.go
+++ ioTracking/ioTracking.go
@@ -13,15 +13,13 @@ func GetNumFiles(
 	ctx *gin.Context) {
 	// Check number of files in directory
 	// and send as json response with unix time
-	t_1 := time.Now().Unix()
+	t := time.Now().Unix()
 	fileNamesObjs, err := os.ReadDir(dirPath)
-	t_2 := time.Now().Unix()
 	if err != nil {
 		ctx.String(http.StatusInternalServerError, "Request error: %s", err.Error())
 		return
 	}
 	num_files := len(fileNamesObjs)
-	t := (t_2 - t_1) / 2
 	jsonResponse := gin.H{
 		"num_files": num_files,
 		"t": t,
