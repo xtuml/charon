3e22b4a4cbca35323287a7ac7aa3fda36819d301
Freddie Mather <freddie.mather@smartdcs.co.uk>
2023-10-20 11:54:45 +0000

    CDSTH-294 Added handling of getting of getting log file location. Testing...

--- downloads/downloads.go
+++ downloads/downloads.go
@@ -1,31 +1,77 @@
 package downloads
 
 import (
+	"errors"
+	"net/http"
 	"os"
 	"strings"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+const (
+	RECEPTION string = "/logs/reception"
+	VERIFIER string = "/logs/verifier"
+)
 
-type fileRequest struct {
-	FileName string `json:"fileName"`
+var inputMap = map[string]string {
+	"RECEPTION": RECEPTION,
+	"VERIFIER": VERIFIER,
+}
+
+func GetStringToStringMap(
+	key string,
+	mapper map[string]string,
+) (string, error) {
+	value, ok := mapper[key]
+	if ok {
+		return value, nil
+	}
+	return "", errors.New("the input key " + key + " does not exist")
 }
 
 
-func PostFileRequest(dirPath string, ctx *gin.Context) {
-	// Add file to request and respond with 200 OK
-	var fileNameObj fileRequest
+type locationFileNameRequest struct {
+	Location string `json:"location" binding:"required"`
+	FilePrefix string `json:"file_prefix" binding:"required"`
+}
+
+
+type fileRequest struct {
+	FileName string `json:"fileName" binding:"required"`
+	Location string `json:"location"`
+}
+
+
+func BindContextBodyJsonToFileRequest(
+	ctx *gin.Context,
+) (fileRequest, error) {
+	// Add file to request
+	var fileRequestObj fileRequest
 
 	// Bind the JSON Data to the fileRequest structure
-	err := ctx.BindJSON(&fileNameObj)
+	err := ctx.BindJSON(&fileRequestObj)
+	return fileRequestObj, err
+}
+
+// Handle bad requests
+func HandleBadRequest(err error, ctx *gin.Context) {
 	if err != nil {
 		ctx.String(http.StatusBadRequest, "Request error: %s", err.Error())
 		return
 	}
+}
+
+
+// Add file to request
+func PostFileRequest(dirPath string, ctx *gin.Context) {
+	var fileNameObj fileRequest
+	fileNameObj, err := BindContextBodyJsonToFileRequest(
+		ctx,
+	)
+	HandleBadRequest(err, ctx)
 	// get file path
-	filePath := dirPath + fileNameObj.FileName 
+	filePath := dirPath + "/" + fileNameObj.FileName 
 	ctx.File(filePath)
 }
 
@@ -52,4 +98,60 @@ func GetFileNames(
 	//fileNameString := "[" + strings.Join(fileNames, ",") + "]"
 	jsonResponse := gin.H{"fileNames": fileNames}
 	ctx.JSON(200, jsonResponse) 
+}
+
+func HandleGetLogFileLocation(
+	dataPath string,
+	location string,
+) (string, error) {
+	
+	folderLocation, err := GetStringToStringMap(
+		location,
+		inputMap,
+	)
+	if err != nil {
+		return "", err
+	}
+	dirPath := dataPath + folderLocation
+	return dirPath, nil
+}
+
+func HandleGetLogFileNames(
+	dataPath string,
+	ctx *gin.Context,
+) {
+	var locationFileObj locationFileNameRequest
+	// Bind the JSON Data to the fileRequest structure
+	err := ctx.BindJSON(&locationFileObj)
+	HandleBadRequest(err, ctx)
+	dirPath, err := HandleGetLogFileLocation(
+		dataPath,
+		locationFileObj.Location,
+	)
+	if err != nil {
+		ctx.String(http.StatusBadRequest, "Request error: %s", err.Error())
+		return
+	}
+	GetFileNames(
+		dirPath,
+		locationFileObj.FilePrefix,
+		ctx,
+	)
+}
+
+func HandleDownloadLogFile(
+	dataPath string,
+	ctx *gin.Context,
+) {
+	fileRequestObj, err := BindContextBodyJsonToFileRequest(
+		ctx,
+	)
+	HandleBadRequest(err, ctx)
+	dirPath, err := HandleGetLogFileLocation(
+		dataPath,
+		fileRequestObj.Location,
+	)
+	HandleBadRequest(err, ctx)
+	filePath := dirPath + "/" + fileRequestObj.FileName
+	ctx.File(filePath)
 }
--- tests/routers/routers_test.go
+++ tests/routers/routers_test.go
@@ -7,10 +7,16 @@ import (
 	"os"
 	"strings"
 	"testing"
+	"encoding/json"
 
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"gitlab.com/smartdcs1/cdsdt/protocol-verifier-http-server/routers"
 )
+
+type jsonFileNames struct {
+	FileNames []string `json:"fileNames"`
+}
 
 func check(e error) {
     if e != nil {
@@ -18,7 +24,8 @@ func check(e error) {
     }
 }
 
-func TestRestartPV(t *testing.T) {
+
+func SetupServerTest() (*gin.Engine, []string) {
 	dname, err := os.MkdirTemp("", "test_dir")
 	check(err)
 	router := routers.SetupRouter(
@@ -40,14 +47,26 @@ func TestRestartPV(t *testing.T) {
 		s := strings.Split(dirName, "/")
 		dirpath := dname
 		for _, dirName := range s {
-			dirpath, err = os.MkdirTemp(dirpath, dirName)
-			check(err)
+			dirpath = dirpath + "/" + dirName
+			err = os.Mkdir(dirpath, 0755)
+			if err != nil {
+				if err.Error() == "mkdir " + dirpath + ": file exists" {
+					continue
+				}
+				check(err)
+			}
 		}
-		check(err)
-		f, err := os.CreateTemp(dirpath, "file")
+		f, err := os.Create(dirpath + "/file" )
+		f.WriteString("test file")
 		check(err)
 		filepaths = append(filepaths, dirpath + "/" + f.Name())
 	}
+	return router, filepaths 
+}
+
+
+func TestRestartPV(t *testing.T) {
+	router, filepaths := SetupServerTest()
 	w := httptest.NewRecorder()
 	var jsonStr = []byte(`{"wait_time":1}`)
 	req, _ := http.NewRequest(
@@ -58,10 +77,108 @@ func TestRestartPV(t *testing.T) {
 	router.ServeHTTP(w, req)
 	assert.Equal(t, 200, w.Code)
 	assert.Equal(t, "Folders Cleaned Successfully", w.Body.String())
+	var err error
 	for _, filepath := range filepaths {
 		_, err = os.Stat(filepath)
 		if os.IsExist(err) {
 			t.Errorf("test_file was not deleted")
 		}
 	} 
+}
+
+func TestDownloadLogFileNamesRECEPTION(t *testing.T) {
+	router, _ := SetupServerTest()
+	w := httptest.NewRecorder()
+	var jsonStr = []byte(`{"location":"RECEPTION","file_prefix":"file"}`)
+	req, _ := http.NewRequest(
+		"POST", 
+		"/download/log-file-names", 
+		bytes.NewBuffer(jsonStr),
+	)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
+	var j jsonFileNames
+    err := json.NewDecoder(w.Body).Decode(&j)
+	check(err)
+	assert.Contains(t, j.FileNames, "file")
+}
+
+func TestDownloadLogFileNamesVERIFIER(t *testing.T) {
+	router, _ := SetupServerTest()
+	w := httptest.NewRecorder()
+	var jsonStr = []byte(`{"location":"VERIFIER","file_prefix":"file"}`)
+	req, _ := http.NewRequest(
+		"POST", 
+		"/download/log-file-names", 
+		bytes.NewBuffer(jsonStr),
+	)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
+	var j jsonFileNames
+    err := json.NewDecoder(w.Body).Decode(&j)
+	check(err)
+	assert.Contains(t, j.FileNames, "file")
+}
+
+func TestDownloadLogFileNamesIncorrectLocation(t *testing.T) {
+	router, _ := SetupServerTest()
+	w := httptest.NewRecorder()
+	var jsonStr = []byte(`{"location":"VERIFIERS","file_prefix":"file"}`)
+	req, _ := http.NewRequest(
+		"POST", 
+		"/download/log-file-names", 
+		bytes.NewBuffer(jsonStr),
+	)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 400, w.Code)
+	assert.Equal(
+		t,
+		"Request error: the input key VERIFIERS does not exist", 
+		w.Body.String(),
+	)
+}
+
+func TestDownloadLogFile(t *testing.T) {
+	router, _ := SetupServerTest()
+	w := httptest.NewRecorder()
+	var jsonStr = []byte(`{"location":"VERIFIER","fileName":"file"}`)
+	req, _ := http.NewRequest(
+		"POST", 
+		"/download/log-file", 
+		bytes.NewBuffer(jsonStr),
+	)
+	router.ServeHTTP(w, req)
+	println(w.Body.String())
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "test file", w.Body.String())
+}
+
+func TestDownloadAerLog(t *testing.T) {
+	router, _ := SetupServerTest()
+	w := httptest.NewRecorder()
+	var jsonStr = []byte(`{"fileName":"file"}`)
+	req, _ := http.NewRequest(
+		"POST", 
+		"/download/aerlog", 
+		bytes.NewBuffer(jsonStr),
+	)
+	router.ServeHTTP(w, req)
+	println(w.Body.String())
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "test file", w.Body.String())
+}
+
+func TestDownloadVerifierLog(t *testing.T) {
+	router, _ := SetupServerTest()
+	w := httptest.NewRecorder()
+	var jsonStr = []byte(`{"fileName":"file"}`)
+	req, _ := http.NewRequest(
+		"POST", 
+		"/download/verifierlog", 
+		bytes.NewBuffer(jsonStr),
+	)
+	router.ServeHTTP(w, req)
+	println(w.Body.String())
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "test file", w.Body.String())
 }
